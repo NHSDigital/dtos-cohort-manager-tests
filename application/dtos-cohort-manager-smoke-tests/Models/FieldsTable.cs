d7a82d7e5b324eff232cfbdde22cdded57e4a1bf
Sarvesh <sarveshwar.balne1@nhs.net>
2025-01-28 14:49:11 +0000

    Initial solution

---
+++ application/dtos-cohort-manager-smoke-tests/Models/FieldsTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dtos_cohort_manager_specflow.Models;
+
+public class FieldsTable
+{
+    public string? FieldName { get; set; }
+    public string? FieldValue { get; set; }
+}
